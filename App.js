26ac91838509378aaa31003a9cb5669cbd6c92f2
Erny <Erny@MacBook-Air-18.local>
2024-03-11 20:47:45 -0700

    commit

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Text } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { useChatClient } from './useChatClient';
+import { AppProvider } from "./AppContext";
+import {
+  Chat,
+  OverlayProvider
+} from 'stream-chat-react-native'; // Or stream-chat-expo
+import { StreamChat } from 'stream-chat';
+import { chatApiKey } from './chatConfig';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const HomeScreen = () => <Text>Home Screen</Text>;
+const chatClient = StreamChat.getInstance(chatApiKey);
+
+
+const NavigationStack = () => {
+  const { clientIsReady } = useChatClient();
+
+  if (!clientIsReady) {
+    return <Text>Loading chat ...</Text>
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <OverlayProvider>
+      <Chat client={chatClient}>
+        <Stack.Navigator>
+          <Stack.Screen name='Home' component={HomeScreen} />
+        </Stack.Navigator>
+      </Chat>
+    </OverlayProvider>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  );
+};
+
+export default () => {
+  return (
+    <AppProvider>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <SafeAreaView style={{ flex: 1 }}>
+          <NavigationContainer>
+            <NavigationStack />
+          </NavigationContainer>
+        </SafeAreaView>
+      </GestureHandlerRootView>
+    </AppProvider>
+  );
+};
---
+++ chatConfig.js
@@ -0,0 +1,5 @@
+// chatConfig.js
+export const chatApiKey = '6cnyzj4ek7u6';
+export const chatUserId = 'user1';
+export const chatUserToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidXNlcjEifQ.2ETAXmYHpUhT8PyZN6_OQDPoAsBIw37fsk3v76Ols84';
+export const chatUserName = 'user1';
